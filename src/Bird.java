69fc6c0212da054c55dedf821339f4f561f810e4
hgentl <hgentle@protonmail.com>
2026-08-16 14:27:36 +0100

    fixed compareTo bug

--- src/Bird.java
+++ src/Bird.java
@@ -104,7 +104,8 @@ public class Bird
      * @return 0 if equal, > 0 if bigger or < 0 if lower
      */
     public int compareTo(Bird bird) {      
-        return getSpecies().compareTo(bird.getSpecies());
+        return getSpecies().compareTo(bird.getSpecies()) &&
+                getLocation().compareTo(bird.getLocation);
     }
     
     /**
